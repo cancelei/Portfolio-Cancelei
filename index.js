ace8b720e64fcc0e3e96c96c53c78d8f8ea8d3f3
Glauber <glauber.bannwart@gmail.com>
2023-05-16 18:52:44 -0300

    JS fix for Linters

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const menu = document.getElementById("menu");
+const menu = document.getElementById('menu');
 const body = document.body;
 
 function menu_open() {
-  menu.classList.add("menu_open");
-  body.classList.add("menu-open");
+  menu.classList.add('menu_open');
+  body.classList.add('menu-open');
 }
 
 function menu_close() {
-  menu.classList.remove("menu_open");
-  body.classList.remove("menu-open");
+  menu.classList.remove('menu_open');
+  body.classList.remove('menu-open');
 }
