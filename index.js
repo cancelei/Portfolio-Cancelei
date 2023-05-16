0a40fafca6264a960f102efd2071263f9d0598bb
Glauber <glauber.bannwart@gmail.com>
2023-05-16 14:43:36 -0300

    JS menu part 1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function open__menu () {
+function menu__open () {
   document.body.classList += "menu__open";
 }
 
 function menu__close() {
   document.body.classList.remove -= "menu__open";
-}	
+}
