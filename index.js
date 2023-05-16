2e224f5e04b12a5e13f2e42984d5add38b30b039
Glauber <glauber.bannwart@gmail.com>
2023-05-16 16:58:49 -0300

    JS menu implementation

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const menu__backdrop = document.getElementsByClassName("menu__backdrop")[0];
-function menu__open() {
-  document.body.classList.add("menu__open");
-  menu__backdrop.addEventListener("click", menu__close);
+const menu = document.getElementById("menu");
+const body = document.body;
+
+function menu_open() {
+  menu.classList.add("menu_open");
+  body.classList.add("menu-open");
 }
 
-function menu__close() {
-  document.body.classList.remove -= "menu__open";
+function menu_close() {
+  menu.classList.remove("menu_open");
+  body.classList.remove("menu-open");
 }
