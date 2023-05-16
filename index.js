868eca9bf12d23e45bd4fcf3965ea98c940c6218
Dany AUGUSTIN <augustindany@hotmail.com>
2023-05-16 14:13:46 -0400

    Fixed Menu issues

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function menu__open () {
-  document.body.classList += "menu__open";
+const menu__backdrop = document.getElementsByClassName("menu__backdrop")[0];
+function menu__open() {
+  document.body.classList.add("menu__open");
+  menu__backdrop.addEventListener("click", menu__close);
 }
 
 function menu__close() {
