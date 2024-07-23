0441eb698ab370cd9f1f7f1b72f5782422aa67bd
cancelei <glauber.bannwart@gmail.com>
2024-07-23 12:19:55 -0300

    add project > epochaid

--- projectsdata.js
+++ projectsdata.js
@@ -1,51 +1,72 @@
 const projectsData = [
   {
-    title: 'To do List',
-    detailsTitle: 'Self',
-    detailsText: 'Front End Dev',
-    detailsYear: '2023',
-    description: 'A minimalistic to-do list app to keep track of your tasks, explore the app to know more!',
-    descriptionLong: 'With a To Do list you can manage your daily tasks. It is simple to use in a way that you can add, delete and mark as done your tasks. You can also filter your tasks by status. It is a great way to keep track of your tasks and be more productive.',
-    imageUrl: 'Assets/portfolioImages/Project To Do List.png',
-    tags: ['HTML', 'CSS', 'Javascript'],
-    githubLink: 'https://github.com/cancelei/bookstore', // Add your GitHub link here
-    liveVersion: 'https://cancelei.github.io/bookstore/', // Add your Live version link here
+    title: "Epoch AId",
+    detailsTitle: "Co-Founder",
+    detailsText: "Rails Full Stack Developer",
+    detailsYear: "2024",
+    description:
+      "Started during a Hackathon, where I found Wey, a Malaysian founder, that asked me to be interim CEO to prepare her for the position in the future. Now it is one of my side project.",
+    descriptionLong:
+      "Connected to AI (ChatGPT API & LLAMA3), Second Opinion Assistant can help Startups with legal demands.",
+    imageUrl: "Assets/portfolioImages/epochaid_jul.png",
+    tags: ["Ruby on Rails", "AI/RAG", "Javascript"],
+    githubLink: "https://github.com/cancelei/epoch_aid", // Add your GitHub link here
+    liveVersion: "https://www.epochaid.com/", // Add your Live version link here
   },
   {
-    title: 'Pokémon APP',
-    detailsTitle: 'React',
-    detailsText: 'Full Stack Dev',
-    detailsYear: '2023',
-    description: 'An app to search for your favorite pokémon, explore the app to know more!',
-    descriptionLong: 'This app is a great way to search for your favorite pokémon. You can interact with other users by commenting on their posts. Share your houghts about your favorite pokémon now.',
-    imageUrl: 'Assets/portfolioImages/Poke 1 - wide.png',
-    tags: ['Webpack', 'APIs', 'Javascript'],
-    githubLink: 'https://github.com/cancelei/Pokemon-APP', // Add your GitHub link here
-    liveVersion: 'https://cancelei.github.io/Pokemon-APP/dist/index.html', // Add your Live version link here
+    title: "To do List",
+    detailsTitle: "Self",
+    detailsText: "Front End Dev",
+    detailsYear: "2023",
+    description:
+      "A minimalistic to-do list app to keep track of your tasks, explore the app to know more!",
+    descriptionLong:
+      "With a To Do list you can manage your daily tasks. It is simple to use in a way that you can add, delete and mark as done your tasks. You can also filter your tasks by status. It is a great way to keep track of your tasks and be more productive.",
+    imageUrl: "Assets/portfolioImages/Project To Do List.png",
+    tags: ["HTML", "CSS", "Javascript"],
+    githubLink: "https://github.com/cancelei/bookstore", // Add your GitHub link here
+    liveVersion: "https://cancelei.github.io/bookstore/", // Add your Live version link here
   },
   {
-    title: 'Space Travelers Hub',
-    detailsTitle: 'React',
-    detailsText: 'Full Stack Dev',
-    detailsYear: '2023',
-    description: 'An app to book your next space travel, explore the app to know more!',
-    descriptionLong: "Space Travelers' Hub is a company that provides commercial and scientific space travel services. They also allow rockets and capsules to be booked. This app is a great way to book your next space travel. You can interact with other users by booking rockets and capsules.",
-    imageUrl: 'Assets/portfolioImages/space rockets.png',
-    tags: ['React', 'Redux', 'Javascript', 'APIs'],
-    githubLink: 'https://github.com/cancelei/Space-Travelers-Hub', // Add your GitHub link here
-    liveVersion: 'https://cancelei.github.io/Space-Travelers-Hub/', // Add your Live version link here
+    title: "Pokémon APP",
+    detailsTitle: "React",
+    detailsText: "Full Stack Dev",
+    detailsYear: "2023",
+    description:
+      "An app to search for your favorite pokémon, explore the app to know more!",
+    descriptionLong:
+      "This app is a great way to search for your favorite pokémon. You can interact with other users by commenting on their posts. Share your houghts about your favorite pokémon now.",
+    imageUrl: "Assets/portfolioImages/Poke 1 - wide.png",
+    tags: ["Webpack", "APIs", "Javascript"],
+    githubLink: "https://github.com/cancelei/Pokemon-APP", // Add your GitHub link here
+    liveVersion: "https://cancelei.github.io/Pokemon-APP/dist/index.html", // Add your Live version link here
   },
   {
-    title: 'NGO/Ruby4Good - Human Essentials',
-    detailsTitle: 'Rails',
-    detailsText: 'Full Stack Dev',
-    detailsYear: '2024',
-    description: 'Participation in an app around an American wide Diaper Bank.',
-    descriptionLong: "Working asynchronously with other developers using the technology that I want to specialize in. Receiving code reviews and interacting with experienced Developers.",
-    imageUrl: 'Assets/portfolioImages/human-essentials.png',
-    tags: ['Rails', 'ERB HTML', 'APIs', 'Postgresql'],
-    githubLink: 'https://github.com/cancelei/rails-human-essentials', // Add your GitHub link here
-    liveVersion: 'humanessentials.app', // Add your Live version link here
+    title: "Space Travelers Hub",
+    detailsTitle: "React",
+    detailsText: "Full Stack Dev",
+    detailsYear: "2023",
+    description:
+      "An app to book your next space travel, explore the app to know more!",
+    descriptionLong:
+      "Space Travelers' Hub is a company that provides commercial and scientific space travel services. They also allow rockets and capsules to be booked. This app is a great way to book your next space travel. You can interact with other users by booking rockets and capsules.",
+    imageUrl: "Assets/portfolioImages/space rockets.png",
+    tags: ["React", "Redux", "Javascript", "APIs"],
+    githubLink: "https://github.com/cancelei/Space-Travelers-Hub", // Add your GitHub link here
+    liveVersion: "https://cancelei.github.io/Space-Travelers-Hub/", // Add your Live version link here
+  },
+  {
+    title: "NGO/Ruby4Good - Human Essentials",
+    detailsTitle: "Rails",
+    detailsText: "Full Stack Dev",
+    detailsYear: "2024",
+    description: "Participation in an app around an American wide Diaper Bank.",
+    descriptionLong:
+      "Working asynchronously with other developers using the technology that I want to specialize in. Receiving code reviews and interacting with experienced Developers.",
+    imageUrl: "Assets/portfolioImages/human-essentials.png",
+    tags: ["Rails", "ERB HTML", "APIs", "Postgresql"],
+    githubLink: "https://github.com/cancelei/rails-human-essentials", // Add your GitHub link here
+    liveVersion: "https://www.humanessentials.app/", // Add your Live version link here
   },
 ];
 
